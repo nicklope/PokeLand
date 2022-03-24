3451d51d8b2e8c63068a322ce9b0956bedcfaebf
Mark Kennedy <kennedymark@hotmail.com>
2022-03-24 13:43:32 -0400

    Created 6 useStates for park sections.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,13 @@ import PokemonCard from '../components/pokemonCard'
 import SectionCard from '../components/sectionCard'
 
 const Home = () => {
+  const [skyPark, setSkyPark] = useState([])
+  const [waterPark, setWaterPark] = useState([])
+  const [forestLand, setForestLand] = useState([])
+  const [volcanoLand, setVolcanoLand] = useState([])
+  const [hauntedHouse, setHauntedHouse] = useState([])
+  const [pokemonRanch, setPokemonRanch] = useState([])
+
   return (
     <div>
       <Landing />
