8a747a60ad0dcfd0c27b60a2e1eff730803156c9
Jason McCoy <jwmccoy29@gmail.com>
2022-03-24 14:23:15 -0500

    update on server

--- server/controller/PokemonController.js
+++ server/controller/PokemonController.js
@@ -4,4 +4,4 @@ const Pokemon = require('../models/Pokemon')
 //const findPokemon = async (req, res) => { try { const {id} = req.params const pokemon = await Pokemon.findById(id) if (pokemon) { return res.status(200).json({pokemon}) } return res.status(404).send('Pokemon with the specified ID does not exist') } catch (error) { return res.status(500).send(error.message) } }
 //const createPokemonDetails = async (req, res) => { try { const pokemon = await new Pokemon(req.body) await pokemon.save() return res.status(200).json({ pokemon }) { catch (error) { return res.status(500).json({ error: error message }) } }
 
-//module.exports = { getPokemonDetails, findPokemon, createPokemon}
+//module.exports = { getPokemonDetails, findPokemon, createPokemon }
