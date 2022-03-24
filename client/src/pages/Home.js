230dcfc09080a84ea37002ea3aaa76cfa59f861f
Mark Kennedy <kennedymark@hotmail.com>
2022-03-24 13:36:21 -0400

    Started Section Card compontents.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react'
 import Landing from '../components/landing'
 import PokemonCard from '../components/pokemonCard'
+import SectionCard from '../components/sectionCard'
 
 const Home = () => {
   return (
     <div>
       <Landing />
       <PokemonCard />
+      <SectionCard />
     </div>
   )
 }
