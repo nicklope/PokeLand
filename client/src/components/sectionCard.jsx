d937919978624102e1276119ab1da559225c927b
Mark Kennedy <kennedymark@hotmail.com>
2022-03-24 16:29:01 -0400

    Importing images

--- client/src/components/sectionCard.jsx
+++ client/src/components/sectionCard.jsx
@@ -1,11 +1,11 @@
-const SectionCard = () => {
+const SectionCard = (props) => {
   return (
     <div className="card" onClick=''>
       <div className="img-wrapper">
-        <img src='https://pm1.narvii.com/7115/ed037afd6a9c1c8c93e4bf8048e34603fe02ed11r1-1024-671v2_hq.jpg' alt="game" />
+        <img src={props.image} alt="game" />
       </div>
       <div className="info-wrapper flex-col">
-        <h3>Section Name</h3>
+        <h3>{props.sectionName}</h3>
         <p>Content</p>
       </div>
     </div>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -32,13 +32,19 @@ const Home = () => {
     <div>
       <Landing />
       <Landing />
-      <section class="sections-container">
-        <SectionCard />
-        <SectionCard />
-        <SectionCard />
-        <SectionCard />
-        <SectionCard />
-        <SectionCard />
+      <section class="container-grid">
+        <SectionCard sectionName="Sky Park" image="" />
+        <SectionCard sectionName="Water Park" image="" />
+        <SectionCard
+          sectionName="Forest Land"
+          image="http://pm1.narvii.com/5725/29631f955694b9f0dde9bec6504ad420d244476e_00.jpg"
+        />
+        <SectionCard
+          sectionName="Volcano Land"
+          image="https://pm1.narvii.com/7115/ed037afd6a9c1c8c93e4bf8048e34603fe02ed11r1-1024-671v2_hq.jpg"
+        />
+        <SectionCard sectionName="Haunted House" image="" />
+        <SectionCard sectionName="Pokemon Ranch" image="" />
       </section>
       <PokemonCard />
     </div>
