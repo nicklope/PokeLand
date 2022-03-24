318248a003a2d530bb022c8d6e75dfb2df9de64c
Jason McCoy <jwmccoy29@gmail.com>
2022-03-24 11:11:09 -0500

    updated controller and packages

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const cors = require('cors')
 const routes = require('./routes')
 const db = require('./db')
 const logger = require('morgan')
+
 const app = express()
+
 const PORT = process.env.PORT || 3001
+
 app.use(express.json())
 app.use(logger('dev'))
 app.use('/api', routes)
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+
+//routes go here
+app.get('/', (request, response) => {
+  response.send({ msg: 'Server Running' })
+})
+
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
