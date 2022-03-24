186a050b2ea1d3d359f4dc4e28226e6dc88ce186
Nick Lopez <nicklopez5454@yahoo.com>
2022-03-24 15:00:22 -0500

    transfered backend files into new local repo

--- server/seed/pokemon.js
+++ server/seed/pokemon.js
@@ -1,15 +1,23 @@
 const db = require('../db')
 const Pokemon = require('../models/Pokemon')
+const axios = require('axios')
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const main = async () => {
-  const pokemon = []
+  let res
+  async function getPokemon() {
+    res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
+    // console.log(res.data.results)
 
-  await Pokemon.insertMany(pokemon)
-  console.log('Pokemon incoming!')
+    console.log('Pokemon incoming!')
+  }
+
+  await getPokemon()
+  await Pokemon.insertMany(res.data.results)
 }
 const run = async () => {
   await main()
   db.close()
 }
+run()
