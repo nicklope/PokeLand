6e7001cab3914803f1e14cadc5665c67693fb262
Jason McCoy <jwmccoy29@gmail.com>
2022-03-24 12:43:16 -0500

    controller update

--- server/controller/PokemonController.js
+++ server/controller/PokemonController.js
@@ -1,5 +1,7 @@
-//const getPokemonDetails = (req, res) => res.json ({})
-//const findPokemon = (req, res) => res.json ({ message: `${req.params.pokemonId}` })
-//const createPokemonDetails = (req, res) => res.json (req.body)
+const Pokemon = require('../models/Pokemon')
+
+//const getPokemonDetails = async (req, res) => { try { const pokemon = await.Pokemon.find() return res.status(200).json({pokemon}) } catch (error) { return res.status(500).send(error.message) } }
+//const findPokemon = async (req, res) => { try { const {id} = req.params const pokemon = await Pokemon.findById(id) if (pokemon) { return res.status(200).json({pokemon}) } return res.status(404).send('Pokemon with the specified ID does not exist') } catch (error) { return res.status(500).send(error.message) } }
+//const createPokemonDetails = async (req, res) => { try { const pokemon = await new Pokemon(req.body) await pokemon.save() return res.status(200).json({ pokemon }) { catch (error) { return res.status(500).json({ error: error message }) } }
 
 //module.exports = { getPokemonDetails, findPokemon, createPokemon}
