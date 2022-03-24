c9b1a18c40d120dd9405e2e281d9c17883a3bcd8
Nick Lopez <nicklopez5454@yahoo.com>
2022-03-24 10:29:46 -0500

    initialized

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const routes = require('./routes')
+const db = require('./db')
+const logger = require('morgan')
+const app = express()
+const PORT = process.env.PORT || 3001
+app.use(express.json())
+app.use(logger('dev'))
+app.use('/api', routes)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
