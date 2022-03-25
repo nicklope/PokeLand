71c269fcbf4e62fec6111d7cd383a7eba2da7890
Mark Kennedy <kennedymark@hotmail.com>
2022-03-24 23:39:44 -0400

    Section Images

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,14 +8,16 @@ import axios from 'axios'
 const Home = () => {
   const navigate = useNavigate()
 
-  // const skyParkImg = ''
-  // const waterParkImg =
-  //   'http://pm1.narvii.com/5725/29631f955694b9f0dde9bec6504ad420d244476e_00.jpg'
-  // const forestLandImg = ''
-  // const volcanoLandImg =
-  //   'https://pm1.narvii.com/7115/ed037afd6a9c1c8c93e4bf8048e34603fe02ed11r1-1024-671v2_hq.jpg'
-  // const hauntedHouseImg = ''
-  // const pokemonRanchImg = ''
+  const skyParkImg =
+    'https://static0.gamerantimages.com/wordpress/wp-content/uploads/2020/06/760771.jpg?q=50&fit=contain&w=943&h=496&dpr=1.5'
+  const waterParkImg = 'https://wallpaperaccess.com/full/118488.jpg'
+  const forestLandImg = 'https://www.1999.co.jp/itbig59/10592095.jpg'
+  const volcanoLandImg =
+    'https://pm1.narvii.com/7115/ed037afd6a9c1c8c93e4bf8048e34603fe02ed11r1-1024-671v2_hq.jpg'
+  const hauntedHouseImg =
+    'https://i.pinimg.com/originals/9d/8e/c2/9d8ec2fbccd833b699cefcb214450a9a.jpg'
+  const pokemonRanchImg =
+    'https://cdn.donmai.us/sample/7c/6a/__whitney_and_miltank_pokemon_and_2_more_drawn_by_nazgul_5511474__sample-7c6abd5f5c0e5954d6846b1d43f02897.jpg'
 
   useEffect(() => {
     // getPokemon()
@@ -31,9 +33,9 @@ const Home = () => {
     }
   }
 
-  // const parkSection = (section) => {
-  //   navigate(`/park/section/${section}`)
-  // }
+  const parkSection = (section) => {
+    navigate(`/park/section/${section}`)
+  }
 
   return (
     <div>
@@ -43,7 +45,7 @@ const Home = () => {
         <SectionCard
           sectionName="Sky Park"
           onclick={() => parkSection('skyPark')}
-          image=""
+          image={skyParkImg}
           // pokemon={skyPark}
         />
         <SectionCard
@@ -55,7 +57,7 @@ const Home = () => {
         <SectionCard
           sectionName="Forest Land"
           onclick={() => parkSection('forestLand')}
-          image=""
+          image={forestLandImg}
           // pokemon={forestLand}
         />
         <SectionCard
@@ -67,13 +69,13 @@ const Home = () => {
         <SectionCard
           sectionName="Haunted House"
           onclick={() => parkSection('hauntedHouse')}
-          image=""
+          image={hauntedHouseImg}
           // pokemon={hauntedHouse}
         />
         <SectionCard
           sectionName="Pokemon Ranch"
           onclick={() => parkSection('pokemonRanch')}
-          image=""
+          image={pokemonRanchImg}
           // pokemon={pokemonRanch}
         />
         <button onClick={() => getPokemon()}>submit</button>
