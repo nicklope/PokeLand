7caa5d8346d9c124984b703f81291488c8a41eb0
Nick Lopez <nicklopez5454@yahoo.com>
2022-03-24 22:05:12 -0500

    Added axios get call to home.js to get data from backend

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -18,13 +18,16 @@ const Home = () => {
   const pokemonRanchImg = ''
 
   useEffect(() => {
-    getPokemon()
+    // getPokemon()
   }, [])
-
+  const getPokemon = async () => {
+    const response = await axios.post('http://localhost:3001/')
+    console.log(response.data)
+  }
   const filterPokemon = (pokemonType) => {
     switch (pokemonType) {
       case '':
-        setSkyPark()
+      // setSkyPark()
     }
   }
 
@@ -41,38 +44,39 @@ const Home = () => {
           sectionName="Sky Park"
           onclick={() => parkSection('skyPark')}
           image=""
-          pokemon={skyPark}
+          // pokemon={skyPark}
         />
         <SectionCard
           sectionName="Water Park"
           onclick={() => parkSection('waterPark')}
           image={waterParkImg}
-          pokemon={waterPark}
+          // pokemon={waterPark}
         />
         <SectionCard
           sectionName="Forest Land"
           onclick={() => parkSection('forestLand')}
           image=""
-          pokemon={forestLand}
+          // pokemon={forestLand}
         />
         <SectionCard
           sectionName="Volcano Land"
           onclick={() => parkSection('volcanoLand')}
           image={volcanoLandImg}
-          pokemon={volcanoLand}
+          // pokemon={volcanoLand}
         />
         <SectionCard
           sectionName="Haunted House"
           onclick={() => parkSection('hauntedHouse')}
           image=""
-          pokemon={hauntedHouse}
+          // pokemon={hauntedHouse}
         />
         <SectionCard
           sectionName="Pokemon Ranch"
           onclick={() => parkSection('pokemonRanch')}
           image=""
-          pokemon={pokemonRanch}
+          // pokemon={pokemonRanch}
         />
+        <button onClick={() => getPokemon()}>submit</button>
       </section>
       <PokemonCard />
     </div>
