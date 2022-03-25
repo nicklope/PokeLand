aa6130b7d29dcc442c26b907d0db1e16c461744b
Mark Kennedy <kennedymark@hotmail.com>
2022-03-25 10:40:49 -0400

    Submit = Homepage

--- client/src/pages/MissionStatement.js
+++ client/src/pages/MissionStatement.js
@@ -2,12 +2,14 @@ import Landing from '../components/landing'
 import Input from '../components/input'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const MissionStatement = () => {
   const backgroundImage =
     'https://poketouch.files.wordpress.com/2018/06/pokemon_trainer_spotlight_professor_oak_with_kanto_starters_bulbasaur_charmander_and_squirtle.jpg'
   const bannerImage =
     'https://fontmeme.com/permalink/220325/8bd71c754139bff2f2b1d2bdaacfe80e.png'
+  const navigate = useNavigate()
 
   const [formValue, setFormValue] = useState({
     name: '',
@@ -24,6 +26,11 @@ const MissionStatement = () => {
       }
     }
     setFormValue(newValues)
+  }
+
+  const handleSubmit = async () => {
+    await axios.post('http://localhost:3001/mission/statement', formValue)
+    navigate('/')
   }
 
   const { name, section, image } = formValue
@@ -56,16 +63,7 @@ const MissionStatement = () => {
           value={image}
           onChange={handleChange}
         />
-        <button
-          onClick={async () =>
-            await axios.post(
-              'http://localhost:3001/mission/statement',
-              formValue
-            )
-          }
-        >
-          Submit
-        </button>
+        <button onClick={() => handleSubmit()}>Submit</button>
       </section>
     </div>
   )
