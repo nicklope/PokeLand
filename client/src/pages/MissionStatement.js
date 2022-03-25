d3aa28b83ba095ed44611d09ef5190269c233937
Mark Kennedy <kennedymark@hotmail.com>
2022-03-24 21:31:34 -0400

    Mission Statemnt form.

--- client/src/pages/MissionStatement.js
+++ client/src/pages/MissionStatement.js
@@ -1,14 +1,51 @@
 import Landing from '../components/landing'
 import Input from '../components/input'
+import { useState, useEffect } from 'react'
 
 const MissionStatement = () => {
+  const [formValue, setFormValue] = useState({
+    pokemonName: '',
+    section: '',
+    image: ''
+  })
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    setFormValue((preState) => {
+      return {
+        ...preState,
+        [name]: value
+      }
+    })
+  }
+
+  const { pokemonName, section, image } = formValue
+
   return (
     <div>
       <Landing />
       <section class="input-section">
-        <Input placeholder="Name" />
-        <Input placeholder="Section" />
-        <Input placeholder="Image" />
+        <input
+          type="text"
+          name="pokemonName"
+          placeholder="Name"
+          value={pokemonName}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="section"
+          placeholder="Section"
+          value={section}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="image"
+          placeholder="Image"
+          value={image}
+          onChange={handleChange}
+        />
         <button>Submit</button>
       </section>
     </div>
