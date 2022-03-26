4d086fe4f8f855bbcccec27a8280d8992519ca8d
Nick Lopez <nicklopez5454@yahoo.com>
2022-03-26 09:43:27 -0500

    made style changes, made imagelink file for front end, cleaned up fron end structure

---
+++ client/src/ImageLinks.js
@@ -0,0 +1,13 @@
+export const backgroundImageLanding =
+  'https://games-b26f.kxcdn.com/wp-content/uploads/2021/07/Pokemon-Wonder-feature-image-770x470.jpg'
+export const backgroundImageAdd =
+  'https://poketouch.files.wordpress.com/2018/06/pokemon_trainer_spotlight_professor_oak_with_kanto_starters_bulbasaur_charmander_and_squirtle.jpg'
+export const mapImage = 'https://i.imgur.com/MQXAmH3.png'
+export const landingBanner =
+  'https://fontmeme.com/permalink/220324/b85ecfe51642a549db4b9889f0f37387.png'
+export const missionBanner =
+  'https://fontmeme.com/permalink/220325/0ef99bc2a694cfa48401526bac9c541c.png'
+export const missionBackground =
+  'https://static0.gamerantimages.com/wordpress/wp-content/uploads/2021/06/pokemon-professors-featured.jpg?q=50&fit=contain&w=943&h=500&dpr=1.5'
+export const mapBanner =
+  'https://fontmeme.com/permalink/220325/88f4258a1aed67434d473062560c4788.png'
